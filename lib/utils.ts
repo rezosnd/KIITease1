0dc9c0ade38f2badcd17eeeb45fe27afc62c3f19
REHAN SUMAN <rehansuman41008@gmail.com>
2025-06-08 22:07:31 +0530

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,15 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+/**
+ * Concatenates and merges Tailwind CSS class names conditionally.
+ * 
+ * Usage:
+ *   cn("p-4", condition && "bg-red-500", otherClassName)
+ * 
+ * @param inputs Any number of class name values, which can be strings, arrays, objects, or falsy values.
+ * @returns A single string of merged Tailwind CSS class names.
+ */
+export function cn(...inputs: ClassValue[]): string {
+  return twMerge(clsx(...inputs))
 }
